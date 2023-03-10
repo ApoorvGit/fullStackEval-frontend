a54ef682e0ae9c05a41572209dcc296e8e483b2b
apoorvGit <apoorvmishra1000@gmail.com>
2023-03-10 22:45:11 +0530

    feat: add routes to pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
+/* eslint-disable react/jsx-wrap-multilines */
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Homepage from './pages/Homepage';
+import CollectionPage from './pages/CollectionPage';
+import ProtectedRoute from './utils/common';
 
 function App() {
   return (
     <div>
-      <h2>Let get started!</h2>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Homepage />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/collection/:contentType"
+            element={
+              <ProtectedRoute>
+                <CollectionPage />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
